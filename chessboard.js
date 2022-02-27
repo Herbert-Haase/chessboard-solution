c0f9ee71a94ab24fea83c7b5e06282986ad9d590
Skelektronik <Acedia_the_Skelektronik@gmx.de>
2022-02-27 13:31:53 +0100

    rename index.html to chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>chessboard various methods</title>
-</head>
-<body>
-    <script>
+
         let size;
         do {
           size = parseInt(prompt("size of the chessboard"));
@@ -33,6 +24,3 @@
             }
         }
         console.log(hashTagLine);
-    </script>
-</body>
-</html>
